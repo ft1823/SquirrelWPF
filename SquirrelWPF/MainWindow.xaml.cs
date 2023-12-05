cc934f426b8bb1ce96d93fbf4ac132168054eb09
Fishel Teitelbaum <ft1823@gmail.com>
2023-12-05 18:34:52 -0500

    1.0.0

--- SquirrelWPF/MainWindow.xaml.cs
+++ SquirrelWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Squirrel;
 
 namespace SquirrelWPF
 {
@@ -20,14 +21,38 @@ namespace SquirrelWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        UpdateManager manager;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnversion_Click(object sender, RoutedEventArgs e)
+        private async void btnversion_Click(object sender, RoutedEventArgs e)
         {
+            await manager.UpdateApp();
 
+            MessageBox.Show("Updated succesfuly!");
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager = await UpdateManager
+               .GitHubUpdateManager(@"https://github.com/ft1823/SquirrelWPF");
+
+            lblVersion.Content = manager.CurrentlyInstalledVersion().ToString();
+
+
+            var updateInfo = await manager.CheckForUpdate();
+
+            if (updateInfo.ReleasesToApply.Count > 0)
+            {
+                lblUpdateavail.Content = "Click to update";
+            }
+            else
+            {
+                lblUpdateavail.Content = "Version is updated";
+            }
         }
     }
 }
